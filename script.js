2f1a86676a027a18acd7de7ee5adb1d06a8644db
Prerana Babar <135982003+preranababar9@users.noreply.github.com>
2023-08-20 15:20:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+function f1(){
+   
+    setTimeout(() => {
+       document.querySelector('.box').style.background="url('g4.jpg') no-repeat  center ";
+ 
+    }, 100);
+    setInterval(() => {
+     document.querySelector('.box').style.background="url('girl2.jpg') no-repeat  center ";
+ }, 3000);
+    setInterval(() => {
+     document.querySelector('.box').style.background="url('girl3.jpg') no-repeat  center ";
+ }, 6000);
+    setInterval(() => {
+     document.querySelector('.box').style.background="url('girl6.png') no-repeat  center ";
+ }, 9000);
+ setInterval(() => {
+    document.querySelector('.box').style.background="url('g1.jpg') no-repeat  center ";
+ }, 12000);
+    setInterval(() => {
+     document.querySelector('.box').style.background="url('girl7.png') no-repeat  center ";
+ }, 15000);
+    setInterval(() => {
+     document.querySelector('.box').style.background="url('girl8.jpeg') no-repeat  center ";
+ }, 18000);
+    setInterval(() => {
+     document.querySelector('.box').style.background="url('girl9.png') no-repeat  center ";
+ }, 21000);
+ setInterval(() => {
+    document.querySelector('.box').style.background="url('g2.jpeg') no-repeat  center ";
+ }, 24000);
+   
+ 
+    };
+    
+    f1();
+ 
+ 
+   function f3(){
+    document.querySelector('.qr').style.display="flex";
+ 
+   }
+   
+   function f4(){
+    document.querySelector('.qr').style.display="none";
+ 
+   }
+   
